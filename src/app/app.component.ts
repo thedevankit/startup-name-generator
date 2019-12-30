94a6a9ec8a16b90a422396657729b49dfdff0639
Ankitkumar Singh <17850047+Anki7singh@users.noreply.github.com>
2019-12-30 23:51:55 +0530

    erereer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'; 
+import { Component, HostListener } from '@angular/core'; 
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 declare function StartupNameGenerator(name):  any;
 
@@ -14,6 +14,8 @@ export class AppComponent {
   public Key;
   public errormsg = false;
   public count;
+  public deferredPrompt: any;
+  public showButton = false;
 
   constructor(private formBuilder:FormBuilder) {
   }
@@ -41,5 +43,33 @@ export class AppComponent {
       this.result = StartupNameGenerator(this.Key);
       this.count = this.result.length;
     }
+  }
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onbeforeinstallprompt(e) {
+    console.log(e);
+    console.log(' function prompt');
+
+    // Prevent Chrome 67 and earlier from automatically showing the prompt
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    this.deferredPrompt = e;
+    this.showButton = true;
+  }
+
+  addToHomeScreen() {
+    this.showButton = false;
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        this.deferredPrompt = null;
+      });
   }
 }
